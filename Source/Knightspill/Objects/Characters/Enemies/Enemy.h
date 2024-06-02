b43abe1c8459268e07a995b6d4703170d13a4aa1
achoruzy <achoruzy@gmail.com>
2024-06-02 19:33:43 +0200

    map updated

---
+++ Source/Knightspill/Objects/Characters/Enemies/Enemy.h
@@ -0,0 +1,21 @@
+﻿// Copyright (C) Data Verft Arkadiusz Choruzy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Knightspill/Game/EngineClassExtensions/BaseCharacter.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class KNIGHTSPILL_API AEnemy : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEnemy();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+};
