f53ca9669b30771b54462e9addf836a567f98484
achoruzy <achoruzy@gmail.com>
2024-06-17 16:18:48 +0200

    fixed pathfinding with navmesh

--- Source/Knightspill/Objects/Characters/Enemies/Enemy.cpp
+++ Source/Knightspill/Objects/Characters/Enemies/Enemy.cpp
@@ -7,7 +7,9 @@
 #include "Knightspill/Objects/Characters/CharacterAttributesComponent.h"
 #include "Knightspill/UI/Floating/Enemy/CharacterHealthBarComponent.h"
 #include "AIController.h"
+#include "Kismet/GameplayStatics.h"
 #include "Navigation/PathFollowingComponent.h"
+#include "NavMesh/NavMeshPath.h"
 
 
 AEnemy::AEnemy()
@@ -24,24 +26,58 @@ AEnemy::AEnemy()
 
 	HealthBarComponent = CreateDefaultSubobject<UCharacterHealthBarComponent>(TEXT("HealthBarComponent"));
 	HealthBarComponent->SetupAttachment(RootComponent);
+
+	LivingStatus = EEnemyLivingStatus::Lives;
+	
+	ShowHealthTimeThreshold = 1 / 30;
 }
 
 
 void AEnemy::BeginPlay()
 {
 	Super::BeginPlay();
-	HealthBarComponent->SetHealthPercent(1.f);
+	Player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
 	EnemyController = Cast<AAIController>(GetController());
+	
+	HealthBarComponent->SetHealthPercent(1.f);
+	HealthBarComponent->SetVisibility(false);
+
+	DistanceToPlayer = FVector::Dist(GetActorLocation(), Player->GetActorLocation());
+
+	if (PatrolTargets.Num() > 0)
+	{
+		GoToTarget(PatrolTargets[0]);
+	}
 }
 
 void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	ShowHealthTimeCurrent += DeltaTime;
+
+	if (ShowHealthTimeCurrent > ShowHealthTimeThreshold)
+	{
+		SetShowHealthBar();
+		ShowHealthTimeCurrent = 0.f;
+	}
 }
 
 void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AEnemy::SetShowHealthBar()
+{
+	DistanceToPlayer = FVector::Dist(GetActorLocation(), Player->GetActorLocation());
+	if (DistanceToPlayer < 1000)
+	{
+		HealthBarComponent->SetVisibility(true);
+	}
+	else
+	{
+		HealthBarComponent->SetVisibility(false);
+	}
 }
 
 float AEnemy::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator,
@@ -50,7 +86,7 @@ float AEnemy::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AC
 	CharacterAttributes->ReceiveDamage(DamageAmount);
 	HealthBarComponent->SetHealthPercent(CharacterAttributes->GetHealthPercent());
 	
-	if (!CharacterAttributes->IsAlive())
+	if (!CharacterAttributes->IsAlive() && LivingStatus == EEnemyLivingStatus::Lives)
 	{
 		Die();
 	}
@@ -64,13 +100,22 @@ void AEnemy::GoToTarget(const AActor* ApproachTarget) const
 		FAIMoveRequest MoveRequest;
 		MoveRequest.SetGoalActor(ApproachTarget);
 		MoveRequest.SetAcceptanceRadius(15.f);
-		
-		EnemyController->MoveTo(MoveRequest);
+		FNavPathSharedPtr NavPath;
+		EnemyController->MoveTo(MoveRequest, &NavPath);
+
+		TArray<FNavPathPoint>& PathPoints = NavPath->GetPathPoints();
+		for (auto& Point : PathPoints)
+		{
+			const FVector& Location = Point.Location;
+			DrawDebugSphere(GetWorld(), Location, 150.f, 24, FColor::Orange, false, 10);
+		}
 	}
 }
 
 void AEnemy::GetHit_Implementation(const int DamageValue, const FVector& DamagePosition, const FVector& DamageNormal)
 {
+	if (!CharacterAttributes->IsAlive()) return;
+	
 	const FVector Forward = GetActorForwardVector();
 	const FVector ToHitPos = (DamagePosition - GetActorLocation()).GetSafeNormal();
 	const FVector FlattenHitPos(ToHitPos.X, ToHitPos.Y, Forward.Z);
@@ -97,9 +142,22 @@ void AEnemy::GetHit_Implementation(const int DamageValue, const FVector& DamageP
 	}
 }
 
-void AEnemy::Die() const
+void AEnemy::Die()
 {
-	PlayAnimationMontage(DeathMontage);
+	switch (int Choice = FMath::RandRange(0, 1))
+	{
+	case 0:
+		LivingStatus = EEnemyLivingStatus::DiedForward;
+		PlayAnimationMontage(DeathMontage, TEXT("DieForward"));
+		break;
+	case 1:
+		LivingStatus = EEnemyLivingStatus::DiedBack;
+		PlayAnimationMontage(DeathMontage, TEXT("DieBack"));
+		break;
+	default:
+		break;
+	}
+	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	HealthBarComponent->SetVisibility(false);
 	// GetMesh()->SetCollisionResponseToAllChannels(ECR_Ignore);
 	// GetMesh()->SetGenerateOverlapEvents(false);
--- Source/Knightspill/Objects/Characters/Enemies/Enemy.h
+++ Source/Knightspill/Objects/Characters/Enemies/Enemy.h
@@ -12,21 +12,42 @@ class AAIController;
 class UCharacterAttributesComponent;
 class UCharacterHealthBarComponent;
 
+UENUM(BlueprintType)
+enum class EEnemyLivingStatus : uint8
+{
+	Lives,
+	DiedForward,
+	DiedBack
+};
+
 UCLASS()
 class KNIGHTSPILL_API AEnemy : public ABaseCharacter, public IHittable
 {
 	GENERATED_BODY()
 
 private:
+	UPROPERTY(EditDefaultsOnly)
+	UCharacterAttributesComponent* CharacterAttributes;
+	UPROPERTY(EditDefaultsOnly)
+	UCharacterHealthBarComponent* HealthBarComponent;
+	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
+	EEnemyLivingStatus LivingStatus;
+	UPROPERTY()
+	ACharacter* Player;
+	UPROPERTY()
+	float DistanceToPlayer;
+	UPROPERTY()
+	float LastDistanceToPlayer;
+	UPROPERTY()
+	float ShowHealthTimeThreshold;
+	UPROPERTY()
+	float ShowHealthTimeCurrent;
+	
 	//** ANIM MONTAGES */
 	UPROPERTY(EditDefaultsOnly, Category="AnimMontages")
 	UAnimMontage* HitReactionMontage;
 	UPROPERTY(EditDefaultsOnly, Category="AnimMontages")
 	UAnimMontage* DeathMontage;
-	UPROPERTY(EditDefaultsOnly)
-	UCharacterAttributesComponent* CharacterAttributes;
-	UPROPERTY(EditDefaultsOnly)
-	UCharacterHealthBarComponent* HealthBarComponent;
 
 	/* AI */
 	UPROPERTY(BlueprintReadOnly, Category="Enemy AI", meta=(AllowPrivateAccess="true"))
@@ -46,6 +67,7 @@ public:
 	
 protected:
 	virtual void BeginPlay() override;
+	void SetShowHealthBar();
 	virtual void GetHit_Implementation(const int DamageValue, const FVector& DamagePosition, const FVector& DamageNormal) override;
-	void Die() const;
+	void Die();
 };
